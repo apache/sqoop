2bbf349f4c59a59a73771c082e49a0b6d01234ce
Andrew Bayer <abayer@apache.org>
2011-07-22 20:03:24 +0000

    MAPREDUCE-918. Test hsqldb server should be memory-only. Contributed by Aaron Kimball.
    From: Thomas White <tomwhite@apache.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sqoop/trunk@1149825 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/sqoop/manager/DefaultManagerFactory.java
+++ src/java/org/apache/hadoop/sqoop/manager/DefaultManagerFactory.java
@@ -41,12 +41,27 @@ public final class DefaultManagerFactory implements ManagerFactory {
 
     String connectStr = options.getConnectString();
 
+    // java.net.URL follows RFC-2396 literally, which does not allow a ':'
+    // character in the scheme component (section 3.1). JDBC connect strings,
+    // however, commonly have a multi-scheme addressing system. e.g.,
+    // jdbc:mysql://...; so we cannot parse the scheme component via URL
+    // objects. Instead, attempt to pull out the scheme as best as we can.
+
+    // First, see if this is of the form [scheme://hostname-and-etc..]
     int schemeStopIdx = connectStr.indexOf("//");
     if (-1 == schemeStopIdx) {
-      // no scheme component?
-      LOG.warn("Could not parse connect string: [" + connectStr
-          + "]; this may be malformed.");
-      return null;
+      // If no hostname start marker ("//"), then look for the right-most ':'
+      // character.
+      schemeStopIdx = connectStr.lastIndexOf(':');
+      if (-1 == schemeStopIdx) {
+        // Warn that this is nonstandard. But we should be as permissive
+        // as possible here and let the ConnectionManagers themselves throw
+        // out the connect string if it doesn't make sense to them.
+        LOG.warn("Could not determine scheme component of connect string");
+
+        // Use the whole string.
+        schemeStopIdx = connectStr.length();
+      }
     }
 
     String scheme = connectStr.substring(0, schemeStopIdx);
@@ -57,6 +72,8 @@ public final class DefaultManagerFactory implements ManagerFactory {
       LOG.warn("Null scheme associated with connect string.");
       return null;
     }
+
+    LOG.debug("Trying with scheme: " + scheme);
 
     if (scheme.equals("jdbc:mysql:")) {
       if (options.isDirect()) {
