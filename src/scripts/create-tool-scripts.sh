7ecb3ee286a0afe79bb8490b8bae2fbdf99bb3a2
Andrew Bayer <abayer@apache.org>
2011-07-22 20:03:56 +0000

    Remove dependency on installed Hadoop for script generation.
    From: Aaron Kimball <aaron@cloudera.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sqoop/trunk@1149912 13f79535-47bb-0310-9956-ffa450edef68

--- src/scripts/create-tool-scripts.sh
+++ src/scripts/create-tool-scripts.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Licensed to Cloudera, Inc. under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -18,15 +18,19 @@
 # Arguments to this script:
 #   The output directory for the result scripts
 #   The path to the script template file.
+#   A file containing the list of tools, one per line.
 # This script should be run in the base directory of the project.
 # It will run 'bin/sqoop help' and determine the names of all subprograms,
 # and then generate wrapper scripts for each of these.
+
+set -e
 
 bin=`dirname $0`
 bin=`cd ${bin} && pwd`
 
 outdir=$1
 template=$2
+toollistfile=$3
 
 if [ -z "$outdir" ]; then
   echo "Output directory is required as the first argument."
@@ -35,6 +39,11 @@ fi
 
 if [ -z "$template" ]; then
   echo "The script template filename is required as the second argument."
+  exit 1
+fi
+
+if [ -z "$toollistfile" ]; then
+  echo "The list of tools is not specified"
   exit 1
 fi
 
@@ -48,8 +57,14 @@ if [ ! -f "$template" ]; then
   exit 1
 fi
 
+if [ ! -f "$toollistfile" ]; then
+  echo "Could not find tool list file: " $toollistfile
+  exit 1
+fi
+
 # Query 'sqoop help' for the list of available commands. 
-subprograms=`bin/sqoop help | awk ' BEGIN {use=0} /^$/ {use=0}  \
+# The output of 'sqoop help' has been written to $toollistfile.
+subprograms=`cat $toollistfile | awk ' BEGIN {use=0} /^$/ {use=0}  \
     { if (use) { print $1 } else { } } /^Available/ {use=1} '`
 
 # For each of these, copy the template into place and sed the
