b666fe1bb6ad37d298838afa5f4436f42d575e9c
Cheolsoo Park <cheolsoo@apache.org>
2012-11-28 13:11:51 -0800

    SQOOP-706 Sqoop won't compile on Java 7 due to new methods in ResultSet interface
    (Jarek Jarcec Cecho via Cheolsoo Park)

--- src/test/com/cloudera/sqoop/testutil/MockResultSet.java
+++ src/test/com/cloudera/sqoop/testutil/MockResultSet.java
@@ -884,6 +884,22 @@ public class MockResultSet implements ResultSet {
       throws SQLException {
   }
 
+  /* Commenting @override as this is addition in java 7 that is not available
+   * in Java 6
+  @Override
+   */
+  public <T> T getObject(int columnIndex, Class<T> type) throws SQLException {
+    return null;
+  }
+
+  /* Commenting @override as this is addition in java 7 that is not available
+   * in Java 6
+  @Override
+   */
+  public <T> T getObject(String columnLabel, Class<T> type) throws SQLException{
+    return null;
+  }
+
   @Override
   public void updateNClob(int columnIndex, Reader reader, long length)
       throws SQLException {
