a43542f7844e06ff87e8d0f05627ba41c626cef2
Jarek Jarcec Cecho <jarcec@apache.org>
2012-12-15 14:59:01 -0800

    SQOOP-782: Update comment to reflect that TestHdfsExtract works fine.
    (Hari Shreedharan via Jarek Jarcec Cecho)

--- execution/mapreduce/src/test/java/org/apache/sqoop/job/TestHdfsExtract.java
+++ execution/mapreduce/src/test/java/org/apache/sqoop/job/TestHdfsExtract.java
@@ -239,8 +239,6 @@ public class TestHdfsExtract extends TestCase {
       };
 
       int numbers = NUMBER_OF_FILES*NUMBER_OF_ROWS_PER_FILE;
-// This test is not currently working due to bug in HdfsExtractor.
-// Check SQOOP-761 for more details.
       assertEquals((1+numbers)*numbers/2, sum);
 
       assertEquals(NUMBER_OF_FILES*NUMBER_OF_ROWS_PER_FILE, index-1);
