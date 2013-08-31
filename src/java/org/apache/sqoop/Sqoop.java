d9ff9e0734245a81dfe15d918f66d8bb62c631fd
Hari Shreedharan <hshreedharan@cloudera.com>
2013-08-30 19:02:41 -0700

    SQOOP-1132.  Print out Sqoop version into log during execution
    (Jarek Jarcec Cecho via Hari Shreedharan)

--- src/java/org/apache/sqoop/Sqoop.java
+++ src/java/org/apache/sqoop/Sqoop.java
@@ -89,6 +89,8 @@ public class Sqoop extends Configured implements Tool {
    * @param opts the SqoopOptions which control the tool's parameters.
    */
   public Sqoop(SqoopTool tool, Configuration conf, SqoopOptions opts) {
+    LOG.info("Running Sqoop version: " + new SqoopVersion().VERSION);
+
     if (null != conf) {
       setConf(conf);
     }
