5270908b3e3c0429855ac316abc7cec52fc3dbf3
Jarek Jarcec Cecho <jarcec@apache.org>
2015-04-08 17:57:57 -0700

    SQOOP-2252: Add default to Avro Schema
    (Andrew Stevenson via Jarek Jarcec Cecho)
    
    (cherry picked from commit 8050107ecf0df6d8dc3869f6c82fe84ee098f08a)

--- src/test/com/cloudera/sqoop/TestParquetImport.java
+++ src/test/com/cloudera/sqoop/TestParquetImport.java
@@ -211,8 +211,8 @@ public class TestParquetImport extends ImportJobTestCase {
   private void checkField(Field field, String name, Type type) {
     assertEquals(name, field.name());
     assertEquals(Type.UNION, field.schema().getType());
-    assertEquals(type, field.schema().getTypes().get(0).getType());
-    assertEquals(Type.NULL, field.schema().getTypes().get(1).getType());
+    assertEquals(Type.NULL, field.schema().getTypes().get(0).getType());
+    assertEquals(type, field.schema().getTypes().get(1).getType());
   }
 
 }
