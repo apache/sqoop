bb7c350d55903f76d441a0950f5237fcfa29f903
Abraham Elmahrek <abe@apache.org>
2015-08-14 15:24:58 -0700

    SQOOP-2492: Sqoop2: getNestedProperties() in MapContext puts key instead of value
    (Banmeet Singh via Abraham Elmahrek)

--- common/src/main/java/org/apache/sqoop/common/MapContext.java
+++ common/src/main/java/org/apache/sqoop/common/MapContext.java
@@ -112,7 +112,7 @@ public class MapContext implements ImmutableContext {
     Map<String, String> subProps = new HashMap<String, String>();
     for(Map.Entry<String, String> entry : options.entrySet()) {
       if(entry.getKey().startsWith(prefix)) {
-        subProps.put(entry.getKey().substring(prefix.length()), entry.getKey());
+        subProps.put(entry.getKey().substring(prefix.length()), entry.getValue());
       }
     }
 
--- common/src/test/java/org/apache/sqoop/common/TestMapContext.java
+++ common/src/test/java/org/apache/sqoop/common/TestMapContext.java
@@ -97,15 +97,16 @@ public class TestMapContext {
   @Test
   public void testGetNestedProperties() {
     Map<String, String> options = new HashMap<String, String>();
-    options.put("sqooptest1", "value");
-    options.put("sqooptest2", "value");
-    options.put("testsqoop1", "value");
+    options.put("sqooptest1", "value1");
+    options.put("sqooptest2", "value2");
     options.put("testsqoop1", "value");
     MapContext mc = new MapContext(options);
     Map<String, String> result = mc.getNestedProperties("sqoop");
     Assert.assertEquals(2, result.size());
     Assert.assertTrue(result.containsKey("test1"));
     Assert.assertTrue(result.containsKey("test2"));
+    Assert.assertEquals(result.get("test1"), "value1");
+    Assert.assertEquals(result.get("test2"), "value2");
     Assert.assertFalse(result.containsKey("testsqoop1"));
     Assert.assertFalse(result.containsKey("testsqoop2"));
   }
