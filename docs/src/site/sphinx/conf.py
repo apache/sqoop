cc3e77b89e653a5f33996d53d3fe4fb7839c16a3
Kate Ting <kathleen@apache.org>
2015-11-18 14:58:57 -0800

    SQOOP-2694: Sqoop2: Doc: Register structure in sphinx for our docs (Jarek Jarcec Cecho via Kate Ting)

--- docs/src/site/sphinx/conf.py
+++ docs/src/site/sphinx/conf.py
@@ -103,12 +103,12 @@ html_use_index = True
 #html_theme = 'default'
 
 html_sidebars = {
-  '**': ['localtoc.html', 'relations.html', 'sourcelink.html'],
+  '**': ['globaltoc.html'],
 }
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
