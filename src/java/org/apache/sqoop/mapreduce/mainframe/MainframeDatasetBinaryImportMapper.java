9d3cc22bb32361a81d29016eacd6266725ca86eb
Chris Teoh <cteoh@cloudera.com>
2017-12-13 09:58:56 +1100

    Added license information

--- src/java/org/apache/sqoop/mapreduce/mainframe/MainframeDatasetBinaryImportMapper.java
+++ src/java/org/apache/sqoop/mapreduce/mainframe/MainframeDatasetBinaryImportMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-=======
->>>>>>> Added support for binary ftp transfers
+
 package org.apache.sqoop.mapreduce.mainframe;
 
 import java.io.IOException;
