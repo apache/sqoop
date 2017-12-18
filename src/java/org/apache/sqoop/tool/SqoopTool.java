eed4364c7a0c8d83c5c912831884722e580e27f8
Szabolcs Vasas <vasas@apache.org>
2017-12-18 11:14:56 +0100

    com.cloudera.sqoop.cli.SqoopParser is not used anymore.

--- src/java/org/apache/sqoop/tool/SqoopTool.java
+++ src/java/org/apache/sqoop/tool/SqoopTool.java
@@ -44,7 +44,7 @@ import org.apache.sqoop.config.ConfigurationHelper;
 
 import com.cloudera.sqoop.SqoopOptions;
 import com.cloudera.sqoop.SqoopOptions.InvalidOptionsException;
-import com.cloudera.sqoop.cli.SqoopParser;
+import org.apache.sqoop.cli.SqoopParser;
 import com.cloudera.sqoop.cli.ToolOptions;
 import com.cloudera.sqoop.tool.ToolDesc;
 
