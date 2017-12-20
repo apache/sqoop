2d8af4894bf676235741b85e71dfc6d417e946f3
Szabolcs Vasas <vasas@apache.org>
2017-12-20 16:02:01 +0100

    Classes from com.cloudera.sqoop.util are moved to org.apache.sqoop.util.

--- src/test/org/apache/sqoop/util/TestOptionsFileExpansion.java
+++ src/test/org/apache/sqoop/util/TestOptionsFileExpansion.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cloudera.sqoop.util;
+package org.apache.sqoop.util;
 
 import java.io.BufferedWriter;
 import java.io.File;
