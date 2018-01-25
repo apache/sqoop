28996336df261e5b3dc1c9dbb8b17f31e60a1a6a
Szabolcs Vasas <vasas@apache.org>
2018-01-25 16:31:15 +0100

    A new constructor is added to HiveServer2ConnectionFactory.

--- src/java/org/apache/sqoop/hive/hiveserver2/HiveServer2ConnectionFactory.java
+++ src/java/org/apache/sqoop/hive/hiveserver2/HiveServer2ConnectionFactory.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,4 +27,9 @@ public class HiveServer2ConnectionFactory extends DriverManagerJdbcConnectionFac
   public HiveServer2ConnectionFactory(String connectionString, String username, String password) {
     super(HS2_DRIVER_CLASS, connectionString, username, password);
   }
-}
+
+  public HiveServer2ConnectionFactory(String connectionString) {
+    this(connectionString, null, null);
+  }
+} 
+  
