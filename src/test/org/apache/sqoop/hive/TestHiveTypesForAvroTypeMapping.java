80a798764d2c4d4158851c4b5b5afc33f31b6aaa
Fero Szabo <fero@cloudera.com>
2018-12-05 14:16:08 +0100

    Fixed review findings and added new test case in TestHiveTypesForAvroTypeMapping

--- src/test/org/apache/sqoop/hive/TestHiveTypesForAvroTypeMapping.java
+++ src/test/org/apache/sqoop/hive/TestHiveTypesForAvroTypeMapping.java
@@ -35,7 +35,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.apache.sqoop.hive.HiveTypes.toHiveType;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.apache.avro.Schema.create;
+import static org.apache.avro.Schema.createEnum;
+import static org.apache.avro.Schema.createFixed;
+import static org.apache.avro.Schema.createUnion;
+import static org.apache.avro.Schema.Type;
 
 @Category(UnitTest.class)
 @RunWith(Parameterized.class)
@@ -46,19 +51,20 @@ public class TestHiveTypesForAvroTypeMapping {
   private final Schema schema;
   private final SqoopOptions options;
 
-  @Parameters(name = "hiveType = {0}, schema = {1}, options = {2}")
+  @Parameters(name = "hiveType = {0}, schema = {1}")
   public static Iterable<? extends Object> parameters() {
     return Arrays.asList(
-        new Object[]{"BOOLEAN", Schema.create(Schema.Type.BOOLEAN), new SqoopOptions()},
-        new Object[]{"INT", Schema.create(Schema.Type.INT), new SqoopOptions()},
-        new Object[]{"BIGINT", Schema.create(Schema.Type.LONG), new SqoopOptions()},
-        new Object[]{"FLOAT", Schema.create(Schema.Type.FLOAT), new SqoopOptions()},
-        new Object[]{"DOUBLE", Schema.create(Schema.Type.DOUBLE), new SqoopOptions()},
-        new Object[]{"STRING", Schema.createEnum("ENUM", "doc", "namespce", new ArrayList<>()), new SqoopOptions()}, // Schema.Type.ENUM
-        new Object[]{"STRING", Schema.create(Schema.Type.STRING), new SqoopOptions()},
-        new Object[]{"BINARY", Schema.create(Schema.Type.BYTES), new SqoopOptions()},
-        new Object[]{"BINARY", Schema.createFixed("Fixed", "doc", "space", 1), new SqoopOptions()},
+        new Object[]{"BOOLEAN", create(Type.BOOLEAN), new SqoopOptions()},
+        new Object[]{"INT", create(Type.INT), new SqoopOptions()},
+        new Object[]{"BIGINT", create(Type.LONG), new SqoopOptions()},
+        new Object[]{"FLOAT", create(Type.FLOAT), new SqoopOptions()},
+        new Object[]{"DOUBLE", create(Type.DOUBLE), new SqoopOptions()},
+        new Object[]{"STRING", createEnum("ENUM", "doc", "namespace", new ArrayList<>()), new SqoopOptions()},
+        new Object[]{"STRING", create(Type.STRING), new SqoopOptions()},
+        new Object[]{"BINARY", create(Type.BYTES), new SqoopOptions()},
+        new Object[]{"BINARY", createFixed("Fixed", "doc", "space", 1), new SqoopOptions()},
         new Object[]{"BINARY", createDecimal(20, 10), new SqoopOptions()},
+        new Object[]{"BINARY", create(Type.BYTES), createSqoopOptionsWithLogicalTypesEnabled()},
         new Object[]{"DECIMAL (20, 10)", createDecimal(20, 10), createSqoopOptionsWithLogicalTypesEnabled()}
         );
   }
@@ -71,12 +77,12 @@ public class TestHiveTypesForAvroTypeMapping {
 
   private static Schema createDecimal(int precision, int scale) {
     List<Schema> childSchemas = new ArrayList<>();
-    childSchemas.add(Schema.create(Schema.Type.NULL));
+    childSchemas.add(create(Type.NULL));
     childSchemas.add(
         LogicalTypes.decimal(precision, scale)
-            .addToSchema(Schema.create(Schema.Type.BYTES))
+            .addToSchema(create(Type.BYTES))
     );
-    return Schema.createUnion(childSchemas);
+    return createUnion(childSchemas);
   }
 
   public TestHiveTypesForAvroTypeMapping(String hiveType, Schema schema, SqoopOptions options) {
