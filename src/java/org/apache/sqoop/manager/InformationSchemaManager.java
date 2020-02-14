1397146de1cd5d3d10cf693657e0a3c9d02e4e45
Yi Wei <Wei.Yi@ubisoft.com>
2020-02-14 15:10:32 +0800

    Oder column names by ordinal position

--- src/java/org/apache/sqoop/manager/InformationSchemaManager.java
+++ src/java/org/apache/sqoop/manager/InformationSchemaManager.java
@@ -52,7 +52,8 @@ public abstract class InformationSchemaManager
     return
       "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS "
     + "WHERE TABLE_SCHEMA = (" + getSchemaQuery() + ") "
-    + "  AND TABLE_NAME = '" + tableName + "' ";
+    + "  AND TABLE_NAME = '" + tableName + "' "
+    + "  ORDER BY ORDINAL_POSITION";
   }
 
   @Override
